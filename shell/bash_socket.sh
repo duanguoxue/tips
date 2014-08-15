f7430ba4d487bcc3a6062815f239172d0e476306
guoxue <duanguoxue@gmail.com>
2014-08-15 20:43:53 +0800

    Update bash_socket.sh

--- shell/bash_socket.sh
+++ shell/bash_socket.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 #author:dgx
 #bash tcp connection
+
+# if already running , exit
+PROC_NUM=$(/usr/sbin/lsof $0 | grep -v COMMAND |wc -l)
+if [ $PROC_NUM -gt 1 ];then
+    echo "already running..."
+    exit
+fi
+
 exec 3<> /dev/tcp/www.zuoyan.net/80
 echo -e "GET / HTTP/1.0\nHOST:www.zuoyan.net\n\n" >&3
 #read the Reply
