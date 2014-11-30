ad79ad4c4f29067e1298255d39442107a664d671
guoxue <duanguoxue@gmail.com>
2014-11-30 19:06:29 +0800

    c++ coverage report shell

---
+++ coverage/gen_coverage_report.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# duanguoxue@gmail.com
+# gen base function code coverage report
+# default open http 8000 port to check report
+# requirement : lcov genthml python 
+
+lcov -b ./ -c -i -d ./ -o .coverage.base
+lcov -b ./ -c -d ./ -o .coverage.run
+lcov -b ./ -d ./ -a .coverage.base -a .coverage.run -o .coverage.total
+
+genhtml .coverage.total -o html_report
+cd html_report && python -m SimpleHTTPServer 8000
